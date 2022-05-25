f62757084e2bdd9f617422094f04cd609368fe01
Ygor Dreyer <ygordreyer@mx2.unisc.br>
2022-05-25 13:22:45 -0300

    Packages and Tech Art Update - HDRP, Adressables, VFX

--- Assets/Scripts/Globals/GameLauncher.cs
+++ Assets/Scripts/Globals/GameLauncher.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using Card.DeckSystem;
 using Fusion;
@@ -225,6 +226,7 @@ namespace FusionExamples.Tanknarok
 				
 			}
 		}*/
+
 
 		private void UpdateUI()
 		{
